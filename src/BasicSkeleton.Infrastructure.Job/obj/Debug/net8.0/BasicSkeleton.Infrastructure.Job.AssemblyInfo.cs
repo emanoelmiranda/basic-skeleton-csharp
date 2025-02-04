d8b90c895c178ef98a5b04885244450bfbb11bd8
emanoelmiranda <el.mirandasilva@gmail.com>
2025-02-04 08:14:17 -0300

    chore: update .gitignore

--- src/BasicSkeleton.Infrastructure.Job/obj/Debug/net8.0/BasicSkeleton.Infrastructure.Job.AssemblyInfo.cs
+++ src/BasicSkeleton.Infrastructure.Job/obj/Debug/net8.0/BasicSkeleton.Infrastructure.Job.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicSkeleton.Infrastructure.Job")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0ea7a99aad4b9503cb858ef134fd36215c6d19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee3020b81dfe3cb815b058b6239b54a17ccb650")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicSkeleton.Infrastructure.Job")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicSkeleton.Infrastructure.Job")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/BasicSkeleton.Infrastructure.Persistence/obj/Debug/net8.0/BasicSkeleton.Infrastructure.Persistence.AssemblyInfo.cs
+++ src/BasicSkeleton.Infrastructure.Persistence/obj/Debug/net8.0/BasicSkeleton.Infrastructure.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicSkeleton.Infrastructure.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0ea7a99aad4b9503cb858ef134fd36215c6d19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee3020b81dfe3cb815b058b6239b54a17ccb650")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicSkeleton.Infrastructure.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicSkeleton.Infrastructure.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
